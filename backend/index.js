e2ce136b3ee11239397dca2760d37133140e4f56
Akash Tandale <akash.tandale@cognived.com>
2022-09-25 13:26:54 +0530

    hot fix

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ app.use(express.json())
 app.use(cors());
 const port = process.env.REACT_APP_PORT;
 
+app.get('/api',(req,res)=>{
+    res.json({
+        message:"Welcome to crawler"
+    });
+})
+
 app.listen(port,()=>{
     console.log(`server running  on  ${port}`);
 })
