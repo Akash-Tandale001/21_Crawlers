d2f0cc6751da438157bdc688a23d7b9881f18388
Harsh Upadhye <harshupadhye2832@gmail.com>
2022-09-26 10:40:48 +0530

    Login route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import * as React from "react";
 import LandingPage from "./Home/Landing_Page";
 import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Login from './Login/Login';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
       </Routes>
       </BrowserRouter>
     </div>
