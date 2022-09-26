1668a3a02dbad3be646204a0e357baa32ee9e9f2
Akash Tandale <ak.tandale123@gmail.com>
2022-09-26 13:20:14 +0530

    added home endpoint

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ app.use(express.json())
 app.use(cors());
 const port = process.env.REACT_APP_PORT;
 
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"Hello welcome by Crawler Team...! "})
+})
 app.use('/api',require('./routes/route'));
 app.listen(port,()=>{
     console.log(`server running  on  ${port}`);
